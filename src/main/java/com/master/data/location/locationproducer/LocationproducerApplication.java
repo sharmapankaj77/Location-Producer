42b5e085b0ee30d26d501a32ab1158131eaeae17
Pankaj Sharma <pankaj.sharma6@ibm.com>
2023-11-28 13:26:30 +0530

    Initial commit for producer service.

---
+++ src/main/java/com/master/data/location/locationproducer/LocationproducerApplication.java
@@ -0,0 +1,13 @@
+package com.master.data.location.locationproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationproducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/master/data/location/locationproducer/LocationproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.data.location.locationproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
